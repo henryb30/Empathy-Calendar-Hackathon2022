9abb8c43e6bf893bcee96345ecbbec85330f44eb
Harmony Petersen <harmonyjpetersen@gmail.com>
2022-02-17 13:52:05 -0800

    Still integrating

--- gCal.js
+++ gCal.js
@@ -4,18 +4,18 @@ const objectInspect = require('object-inspect')
 const ApiCalendar = require('react-google-calendar-api')
 const gcalSlack = require('gcal-slack-integration')
 
-module.exports.createEvent = function createEvent(event, unknown){
-    
-}
-
 module.exports.fetchUsers = function fetchUsers(users, unknown){
     
 }
 
-module.exports.fetchMeetings = function fetchPotentialConflicts(googleUsers, unknown){
+module.exports.fetchMeetings = function fetchUserCalendar(googleUsers, unknown){
 
 }
 
 module.exports.createMeeting = function createMeeting(googleUsers, unknown){
 
+}
+
+module.exports.checkHealth = function checkHealth(){
+    google.getSupportedAPIs()
 }
