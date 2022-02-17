778e477c8d4df5da5a013e4c54132c2ab107f70c
Harmony Petersen <harmonyjpetersen@gmail.com>
2022-02-17 09:09:11 -0800

    Grabbing initial libraries, putting together start of basic skeleton.

---
+++ gCal.js
@@ -0,0 +1,16 @@
+const { google } = require('googleapis')
+const _ = require('lodash')
+const objectInspect = require('object-inspect')
+const ApiCalendar = require('react-google-calendar-api')
+
+module.exports.createEvent = function createEvent(event){
+
+}
+
+module.exports.fetchUsers = function fetchUsers(users){
+    
+}
+
+module.exports.fetchMeetings = function fetchMeetings(googleUsers){
+
+}
