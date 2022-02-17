4cf12cd395f8b79a14cc3833597bbb06d66ac8d9
Harmony Petersen <harmonyjpetersen@gmail.com>
2022-02-17 09:21:32 -0800

    Continuing initial skeleton

--- gCal.js
+++ gCal.js
@@ -3,14 +3,18 @@ const _ = require('lodash')
 const objectInspect = require('object-inspect')
 const ApiCalendar = require('react-google-calendar-api')
 
-module.exports.createEvent = function createEvent(event){
+module.exports.createEvent = function createEvent(event, unknown){
 
 }
 
-module.exports.fetchUsers = function fetchUsers(users){
+module.exports.fetchUsers = function fetchUsers(users, unknown){
     
 }
 
-module.exports.fetchMeetings = function fetchMeetings(googleUsers){
+module.exports.fetchMeetings = function fetchPotentialConflicts(googleUsers, unknown){
+
+}
+
+module.exports.createMeeting = function createMeeting(googleUsers, unknown){
 
 }
