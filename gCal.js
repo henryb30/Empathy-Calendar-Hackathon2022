97051ba6ffd6cbc78a785a2c6442a9e73c1bc9fc
Harmony Petersen <harmonyjpetersen@gmail.com>
2022-02-17 14:28:49 -0800

    Still working through gcal integration.

--- gCal.js
+++ gCal.js
@@ -3,6 +3,11 @@ const _ = require('lodash')
 const objectInspect = require('object-inspect')
 const ApiCalendar = require('react-google-calendar-api')
 const gcalSlack = require('gcal-slack-integration')
+const oauth2Client = new google.auth.OAuth2(
+    credentials.web.client_id,
+    credentials.web.client_secret,
+    credentials.web.redirect_uris[0],
+);
 
 module.exports.fetchUsers = function fetchUsers(users, unknown){
     
